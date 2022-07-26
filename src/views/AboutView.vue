0f3af155c790d71cedbac18ceedb140fe5f5ff39
j31234 <j31234@126.com>
2022-07-26 16:53:19 +0800

    monaco editor demo: in AbortView

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,16 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <div id="container" style="height: 600px; text-align: left;"></div>
   </div>
 </template>
+
+<script>
+export default {
+  mounted () {
+    this.$monaco.editor.create(document.getElementById('container'), {
+      value: ['function x() {', '\tconsole.log("Hello world!");', '}'].join('\n'),
+      language: 'javascript'
+    })
+  }
+}
+</script>
