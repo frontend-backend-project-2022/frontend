8e8b721d0b2aa530d79a6ec9bd972727af203a99
j31234 <j31234@126.com>
2022-07-29 11:29:46 +0800

    fix: monaco editor autocompletion won't work

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import EditorView from '../views/EditorView.vue'
 
 const routes = [
   {
@@ -10,7 +11,7 @@ const routes = [
   {
     path: '/editor',
     name: 'editor',
-    component: () => import('../views/EditorView.vue') // lazy load
+    component: EditorView
   },
   {
     path: '/terminal',
--- vue.config.js
+++ vue.config.js
@@ -2,10 +2,11 @@ const { defineConfig } = require('@vue/cli-service')
 const MonacoWebpackPlugin = require('monaco-editor-webpack-plugin')
 
 module.exports = defineConfig({
-  transpileDependencies: true,
   configureWebpack: {
-    plugins: [new MonacoWebpackPlugin({
-      languages: ['javascript', 'python']
-    })]
+    plugins: [
+      new MonacoWebpackPlugin({
+        languages: ['javascript', 'python', 'typescript']
+      })
+    ]
   }
 })
